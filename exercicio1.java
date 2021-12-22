3c80ab1a42e024705818f0a320562184f64bee12
unknown <dleitedias@gmail.com>
2021-12-22 11:11:48 -0300

    Lista de exercícios 4

---
+++ exercicio1.java
@@ -0,0 +1,30 @@
+package lista4;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+		/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, 
+		 * um valor por linha, inclusive o X, se for o caso.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x, i;
+		
+		System.out.println("Digite um número: ");
+		x = sc.nextInt();
+		
+		for (i = 1; i <= x; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+		
+	}
+
+}
