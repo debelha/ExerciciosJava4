f7d5a9b8340da0fc9bc672c802b0f00f9347a5c5
unknown <dleitedias@gmail.com>
2021-12-27 18:31:01 -0300

    Lista de exercícios 4

---
+++ exercicio6.java
@@ -0,0 +1,28 @@
+package lista4;
+
+import java.util.Scanner;
+
+public class exercicio6 {
+
+	public static void main(String[] args) {
+		
+		//Ler um número inteiro N e calcular todos os seus divisores.
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N, i;
+		
+		System.out.println("Digite um número: ");
+		N = sc.nextInt();
+		
+		for (i=1; i<=N; i++) {
+			if (N % i == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+		
+	}
+
+}
